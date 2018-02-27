f2440bddc742dccf260a9a4d50a86fefa4a0cde6
Shaina Mae Mainar <shainamainar@gmail.com>
2018-02-27 16:41:38 -0500

    Initial commit

---
+++ app/src/main/java/csci490/cofc/edu/gitreference_mainar/GitReference.java
@@ -0,0 +1,48 @@
+package csci490.cofc.edu.gitreference_mainar;
+
+/**
+ * Created by smainar on 2/22/2018.
+ */
+
+public class GitReference {
+    String command;
+    String example;
+    String explanation;
+    String section;
+
+    public String getCommand() {
+        return command;
+    }
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
+
+    public String getExample() {
+        return example;
+    }
+
+    public void setExample(String example) {
+        this.example = example;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public GitReference(){
+
+    }
+}
